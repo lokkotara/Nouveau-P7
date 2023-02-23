e263602b9fe8a4031b20ebd54a778cdea3138c26
greg <lokkotarakun@gmail.com>
2023-02-23 11:21:35 +0100

    hotfix for GP version

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import initPage from "./pages/index.js";
 import { setDataManagerSource } from "./dataManager.js";
 
-setDataManagerSource("./datas/recipes.json");
+setDataManagerSource("https://lokkotara.github.io/P7_13012023/datas/recipes.json");
+// setDataManagerSource("http://127.0.0.1:5500/datas/recipes.json");
 initPage();
