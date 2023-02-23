30330a7490f7e0d5d942591cb80cf9fc0cfd84c5
greg <lokkotarakun@gmail.com>
2023-02-23 10:25:42 +0100

    fix: hotfix for github pages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import initPage from "./pages/index.js";
 import { setDataManagerSource } from "./dataManager.js";
 
-setDataManagerSource("http://localhost:5500/datas/recipes.json");
+setDataManagerSource("/datas/recipes.json");
 initPage();
